cd6f61f348ed30fbbb87f7c392a1e0263fe4d050
sherlyastha <asthapandeycse9@gmail.com>
2023-05-31 22:58:15 +0530

    ft. authentication

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,27 +1,48 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+import { useRouter } from "next/navigation";
+import { FaUserAlt } from "react-icons/fa";
+import { useSupabaseClient } from '@supabase/auth-helpers-react';
+import { toast } from "react-hot-toast";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
+
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
+
+
 import Button from "./Button";
 
 interface HeaderProps {
-    children: React.ReactNode;
-    className?: string;
-  }
+  children: React.ReactNode;
+  className?: string;
+}
 
-const Header:React.FC<HeaderProps> = ({
-    children,
-    className
+const Header: React.FC<HeaderProps> = ({
+  children,
+  className,
 }) => {
+  
+  const router = useRouter();
+  const authModal = useAuthModal();
 
-    const router = useRouter();
+  const supabaseClient = useSupabaseClient();
+  const { user } = useUser();
 
-    const handleLogout =() =>{
-        //handle logout ater
+  const handleLogout = async () => {
+    const { error } = await supabaseClient.auth.signOut();
+    // resetting the player on logout
+    router.refresh();
+
+    if (error) {
+      toast.error(error.message);
     }
+    else {
+      toast.success("Logged Out!")
+    }
+  }
 
   return (
     <div
@@ -33,9 +54,9 @@ const Header:React.FC<HeaderProps> = ({
         `,
         className
       )}>
-        <div className="w-full mb-4 flex items-center justify-between">
+      <div className="w-full mb-4 flex items-center justify-between">
         <div className="hidden md:flex gap-x-2 items-center">
-        <button 
+          <button 
             onClick={() => router.back()} 
             className="
               rounded-full 
@@ -51,7 +72,7 @@ const Header:React.FC<HeaderProps> = ({
             <RxCaretLeft className="text-white" size={35} />
           </button>
           <button 
-            onClick={() => router.back()} 
+            onClick={() => router.forward()} 
             className="
               rounded-full 
               bg-black 
@@ -67,7 +88,7 @@ const Header:React.FC<HeaderProps> = ({
           </button>
         </div>
         <div className="flex md:hidden gap-x-2 items-center">
-        <button 
+          <button 
             onClick={() => router.push('/')} 
             className="
               rounded-full 
@@ -82,8 +103,8 @@ const Header:React.FC<HeaderProps> = ({
             "
           >
             <HiHome className="text-black" size={20} />
-        </button>
-        <button 
+          </button>
+          <button 
             onClick={() => router.push('/search')} 
             className="
               rounded-full 
@@ -98,28 +119,53 @@ const Header:React.FC<HeaderProps> = ({
             "
           >
             <BiSearch className="text-black" size={20} />
-        </button>
+          </button>
         </div>
         <div className="flex justify-between items-center gap-x-4">
+          {user ? (
+            <div className="flex gap-x-4 items-center">
+              <Button 
+                onClick={handleLogout} 
+                className="bg-white px-6 py-2"
+              >
+                Logout
+              </Button>
+              <Button 
+                onClick={() => router.push('/account')} 
+                className="bg-white"
+              >
+                <FaUserAlt />
+              </Button>
+            </div>
+          ) : (
             <>
-                <div>
-                    <Button>
-                        Sign up Miss!
-                    </Button>
-                </div>
-                <div>
-                    <Button 
-                    onClick={()=>{}}
-                    className="bg-white px-6 py-2">
-                        Login Miss!
-                    </Button>
-                </div>
+              <div>
+                <Button 
+                  onClick={authModal.onOpen} 
+                  className="
+                    bg-transparent 
+                    text-neutral-300 
+                    font-medium
+                  "
+                >
+                  Sign up
+                </Button>
+              </div>
+              <div>
+                <Button 
+                  onClick={authModal.onOpen} 
+                  className="bg-white px-6 py-2"
+                >
+                  Log in
+                </Button>
+              </div>
             </>
+          )}
         </div>
-        </div>
-        {children}
+      </div>
+      {children}
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
